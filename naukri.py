1e89a775664a76c978d0a5a8c8ff6b9a2a037192
zaidkaladia <kaladiazaid@gmail.com>
2024-02-23 02:30:31 +0530

    collected the data from naukri.com (title,companyName,skills and URLs) but havent stored it anywhere

--- naukri.py
+++ naukri.py
@@ -11,45 +11,56 @@ import pandas as pd
 
 
 def scrapeNaukriDotCom(title="", experience=0, location = ""):
+
+
+    listings = {"title": [],
+            "companyName": [],
+            "skills": [],
+            "jobURL": []
+            }
+            
     titleSEOKey = title.replace(" ", "-")
     title = title.replace(" ", "%20")
+    i=1
+    testURL = f"https://www.naukri.com/jobapi/v3/search?noOfResults=20&urlType=search_by_key_loc&searchType=adv&location={location}&keyword={title}&pageNo={i}&experience={experience}&k={title}&l={location}&experience={experience}&seoKey={titleSEOKey}-jobs-in-{location}&src=jobsearchDesk&latLong="
+
     
-    testURL = f"https://www.naukri.com/jobapi/v3/search?noOfResults=20&urlType=search_by_key_loc&searchType=adv&location={location}&keyword={title}&pageNo=1&experience={experience}&k={title}&l={location}&experience={experience}&seoKey={titleSEOKey}-jobs-in-{location}&src=jobsearchDesk&latLong="
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+#        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
         "AppId" : "109",
         "SystemId" : "Naukri"
     }
-    # resp = httpx.get(URL, headers=headers)
     resp = requests.get(testURL, headers=headers)
-    with open("foo2.json", 'w') as f:
-        json.dump(resp.json(), f)
     respJson = resp.json()
-    # print(respJson)
-
-    listings = {"title": [],
-                "companyName": [],
-                "skills": [],
-                "jobURL": []
-                }
- 
-    jobDetails = respJson["jobDetails"]
-    for jobDetail in jobDetails:
-        try:
-            jobDetailTitle = jobDetail["title"]
-            jobDetailCompanyName = jobDetail["companyName"]
-            jobDetailTagsAndSkills = jobDetail["tagsAndSkills"]
-            jobDetailJobURL = jobDetail["jdURL"]
-        except KeyError as err:
-            continue
-        listings["title"].append(jobDetailTitle)
-        listings["companyName"].append(jobDetailCompanyName)
-        listings["skills"].append(jobDetailTagsAndSkills)
-        listings["jobURL"].append(jobDetailJobURL)
+    while resp.status_code==200:
+        jobDetails = respJson["jobDetails"]
+        for jobDetail in jobDetails:
+            try:
+                jobDetailTitle = jobDetail["title"]
+                jobDetailCompanyName = jobDetail["companyName"]
+                jobDetailTagsAndSkills = jobDetail["tagsAndSkills"]
+                jobDetailJobURL = jobDetail["jdURL"]
+            except KeyError as err:
+                continue
+            listings["title"].append(jobDetailTitle)
+            listings["companyName"].append(jobDetailCompanyName)
+            listings["skills"].append(jobDetailTagsAndSkills)
+            listings["jobURL"].append(jobDetailJobURL)
+        i=i+1
+        testURL = f"https://www.naukri.com/jobapi/v3/search?noOfResults=20&urlType=search_by_key_loc&searchType=adv&location={location}&keyword={title}&pageNo={i}&experience={experience}&k={title}&l={location}&experience={experience}&seoKey={titleSEOKey}-jobs-in-{location}&src=jobsearchDesk&latLong="
+        resp = requests.get(testURL, headers=headers)
+        respJson = resp.json()
+        
+        
+        
+        
+            
+            
+            
     df = pd.DataFrame(listings)
     print(df)
 
     
 
 
-scrapeNaukriDotCom("web develoment", 0, "vadodara")
+scrapeNaukriDotCom("web development",0, "vadodara")
