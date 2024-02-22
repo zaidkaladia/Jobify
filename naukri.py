4c2c61cdaa257bacda646d7df8f112700fe75cba
Deep-Kumawat <deepdkmail@gmail.com>
2024-02-22 18:02:55 +0530

    opiniated url

--- naukri.py
+++ naukri.py
@@ -9,9 +9,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-def scrapeNaukriDotCom():
+def scrapeNaukriDotCom(title="", experience="", location = ""):
     URL = "https://www.naukri.com/jobapi/v3/search?noOfResults=20&urlType=search_by_key_loc&searchType=adv&location=vadodara&keyword=data%20analyst&pageNo=1&experience=0&k=data%20analyst&l=vadodara&experience=0&seoKey=data-analyst-jobs-in-vadodara&src=jobsearchDesk&latLong="
-
+    
+    titleSEOKey = title.replace(" ", "-")
+    title = title.replace(" ", "%20")
+    
+    testURL = f"https://www.naukri.com/jobapi/v3/search?noOfResults=20&urlType=search_by_key_loc&searchType=adv&location={location}&keyword={title}&pageNo=1&experience={experience}&k={title}&l={location}&experience={experience}&seoKey={titleSEOKey}-jobs-in-{location}&src=jobsearchDesk&latLong="
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
         "AppId" : "109",
@@ -28,7 +32,7 @@ def scrapeNaukriDotCom():
                 "skills": [],
                 "jobURL": []
                 }
-
+ 
     jobDetails = respJson["jobDetails"] 
     for jobDetail in jobDetails:
         listings["title"].append(jobDetail["title"])
