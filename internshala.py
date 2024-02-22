652e55357b96b7a9cff703b31d23d0690cfd8eae
zaidkaladia <kaladiazaid@gmail.com>
2024-02-22 23:38:07 +0530

    collected the data from internshala (title,companyName,skills and URLs) but havent stored it anywhere

--- internshala.py
+++ internshala.py
@@ -15,38 +15,38 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 }
 
-def scarpeWithSession(URL):
-    session = HTMLSession()
-    r = session.get(URL)
-    r.html.render(sleep=2, keep_page=True)
-    skills = r.html.find("#select_category")
-    for skill in skills:
-        print(skill.text)
-        
-def launchBrowser(URL):
-    def closeIntershalaPopUp():
-        closePopUpButton = driver.find_element(by=By.ID, value="close_popup")
-        closePopUpButton.click()
-    options = Options()
-    options.headless = True
-    driver = webdriver.Chrome(options=options)
-    driver.get(URL)
-    closePopUpButton = driver.find_element(by=By.ID, value="close_popup")
-    closePopUpButton.click()
-    driver.refresh()
-    # sleep(1500)
-    try:
-        closePopUpButton = driver.find_element(by=By.ID, value="close_popup")
-        closePopUpButton.click()
-    except:
-        print("pop up was not displayed after refresh")
-    skillsSelectBox = driver.find_element(by=By.ID, value="categoryOptions")
-    skillsSelectBox.click()
-    driver.implicitly_wait(1)
-    selectTag = driver.find_element(by=By.ID, value="select_category")
-    selectTagOptions = selectTag.find_elements(by=By.XPATH, value="*")
-    for selectTagOption in selectTagOptions:
-        print(selectTagOption.get_attribute("outerHTML"))
+#def scarpeWithSession(URL):
+#    session = HTMLSession()
+#    r = session.get(URL)
+#    r.html.render(sleep=2, keep_page=True)
+#    skills = r.html.find("#select_category")
+#    for skill in skills:
+#        print(skill.text)
+#        
+#def launchBrowser(URL):
+#    def closeIntershalaPopUp():
+#        closePopUpButton = driver.find_element(by=By.ID, value="close_popup")
+#        closePopUpButton.click()
+#    options = Options()
+#    options.headless = True
+#    driver = webdriver.Chrome(options=options)
+#    driver.get(URL)
+#    closePopUpButton = driver.find_element(by=By.ID, value="close_popup")
+#    closePopUpButton.click()
+#    driver.refresh()
+#    # sleep(1500)
+#    try:
+#        closePopUpButton = driver.find_element(by=By.ID, value="close_popup")
+#        closePopUpButton.click()
+#    except:
+#        print("pop up was not displayed after refresh")
+#    skillsSelectBox = driver.find_element(by=By.ID, value="categoryOptions")
+#    skillsSelectBox.click()
+#    driver.implicitly_wait(1)
+#    selectTag = driver.find_element(by=By.ID, value="select_category")
+#    selectTagOptions = selectTag.find_elements(by=By.XPATH, value="*")
+#    for selectTagOption in selectTagOptions:
+#        print(selectTagOption.get_attribute("outerHTML"))
     # print("selectTagOptions: ", selectTagOptions)
 
     # while True:
@@ -57,21 +57,22 @@ def launchBrowser(URL):
 def scrapeInternshala(profile, location):
     # for now we are only supporting profile and location
     # profile is a mendatory argument
-    profile = profile.strip().lower().replace(' ', '-') + "-internship/"
+    profile = profile.strip().lower().replace(' ', '-')
     location = location.strip().lower().replace(' ', '-')
     if(location != ''):
-        URL = f"https://internshala.com/internships/work-from-home-{profile}-internships-in-{location}/part-time-true/"
+        URL = f"https://internshala.com/internships/work-from-home-{profile}-internships-in-{location}/part-time-false/"
     else:
-        URL = f"https://internshala.com/internships/work-from-home-{profile}-internships/part-time-true/"
+        URL = f"https://internshala.com/internships/work-from-home-{profile}-internships/part-time-false/"
     
     listings = {"title": [],
                 "companyName": [],
                 "skills": [],
+                "URLs":[],
                 }
     resp = httpx.get(URL, headers=headers)
     soup = BeautifulSoup(resp, 'html.parser')
     numberOfPages = int(soup.find(id="total_pages").text)
-    print(numberOfPages)
+#    print(numberOfPages)
     for i in range(1, numberOfPages+1):
         URL = URL + f"page-{i}/"
         resp = httpx.get(URL, headers=headers)
@@ -93,9 +94,14 @@ def scrapeInternshala(profile, location):
                 internshipListingCardTitleAndCompany = internshipListingCard.div.find(class_ = "individual_internship_header").find(class_="company")
                 internshipListingCardTitle = internshipListingCardTitleAndCompany.find(class_="heading_4_5 profile")
                 internshipListingCardCompanyName = internshipListingCardTitleAndCompany.find(class_="heading_6 company_name")
+
+                internshipListingCardURL = internshipListingCard.find(class_="btn btn-secondary view_detail_button_outline").get('href')
+
+                
                 
                 listings["title"].append(internshipListingCardTitle.text.strip())
                 listings["companyName"].append(internshipListingCardCompanyName.text.strip())
+                listings["URLs"].append(internshipListingCardURL)
                 
                 #Getting skills
                 internshipListingCardDetailsPageLink = "https://internshala.com" + internshipListingCard.find(class_="button_container_card").div.a['href']
@@ -117,7 +123,7 @@ def scrapeInternshala(profile, location):
             print(err)
     df = pd.DataFrame(listings)
     print(df)
-scrapeInternshala("web development", "vadodara")
+scrapeInternshala("Web Development", "Ahmedabad")
 # launchBrowser(URL)
 # resp = httpx.get(URL, headers=headers)
 # html = HTMLParser(resp.text)
