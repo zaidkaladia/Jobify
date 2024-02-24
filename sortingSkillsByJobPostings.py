07e04937caacfc83eaadbd686ea8a5cb58a55440
zaidkaladia <kaladiazaid@gmail.com>
2024-02-24 08:11:37 +0530

    added a top in demand skill finder sortingSkillsByJobPostings.py output in json form

--- sortingSkillsByJobPostings.py
+++ sortingSkillsByJobPostings.py
@@ -1,25 +1,29 @@
-# importing the module
 import pandas as pd
 import operator
+import json
 
- 
-
-df = pd.read_csv("data.csv", usecols = ['skills', 'URLs'])
+df = pd.read_csv("data.csv", usecols=['skills', 'URLs'])
 skillCountDictionary = {}
-totalOutputs=0
+totalOutputs = 0
+
 for index, row in df.iterrows():
-#    row['skills'][1] - row['skills'][len(row['skills'])-2]
-    totalOutputs = totalOutputs+1
-    if row['URLs'][12]=="n":
-            listedSkills = row['skills'][2:-2].split(",")
+    totalOutputs += 1
+    if row['URLs'][12] == "n":
+        listedSkills = row['skills'][2:-2].split(",")
     else:
         listedSkills = row['skills'][2:-2].split("', '")
+    
     for skill in listedSkills:
         try:
             skillCountDictionary[skill] += 1
-            
         except:
             skillCountDictionary[skill] = 1
+
 sorted_x = sorted(skillCountDictionary.items(), key=operator.itemgetter(1))
-print(sorted_x)
+
+# Convert the sorted dictionary to a JSON string
+json_output = json.dumps(sorted_x)
+
+print(json_output)
 print(totalOutputs)
+
