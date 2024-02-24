5903d8f24e9fdc612f4dd65fe90f9c1c94f750a6
zaidkaladia <kaladiazaid@gmail.com>
2024-02-24 07:57:22 +0530

    added a top in demand skill finder sortingSkillsByJobPostings.py

--- sortingSkillsByJobPostings.py
+++ sortingSkillsByJobPostings.py
@@ -6,10 +6,14 @@ import operator
 
 df = pd.read_csv("data.csv", usecols = ['skills', 'URLs'])
 skillCountDictionary = {}
-
+totalOutputs=0
 for index, row in df.iterrows():
 #    row['skills'][1] - row['skills'][len(row['skills'])-2]
-    listedSkills = row['skills'][2:-2].split("', '")
+    totalOutputs = totalOutputs+1
+    if row['URLs'][12]=="n":
+            listedSkills = row['skills'][2:-2].split(",")
+    else:
+        listedSkills = row['skills'][2:-2].split("', '")
     for skill in listedSkills:
         try:
             skillCountDictionary[skill] += 1
@@ -18,3 +22,4 @@ for index, row in df.iterrows():
             skillCountDictionary[skill] = 1
 sorted_x = sorted(skillCountDictionary.items(), key=operator.itemgetter(1))
 print(sorted_x)
+print(totalOutputs)
